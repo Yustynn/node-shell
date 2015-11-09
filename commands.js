15402680a6eb04bcde34b1fd6a90fee7e4c94f2d
Nick <npalenchar@gmail.com>
2015-11-09 17:34:56 -0500

    up to sort

--- commands.js
+++ commands.js
@@ -5,10 +5,12 @@ var commands = {
   date: function(inputs) { parseResponse(Date()); },
   ls: ls,
   echo: echo,
-  default: function(inputs) { parseResponse('You typed: ' + inputs.join(' ')); }
+  cat: cat,
+  head: head,
+  tail: tail,
+  sort: sort,
+  default: function(inputs) { parseResponse('You typed: ' + inputs.join(' '));}
 }
-
-
 
 function parseCmd(cmd) {
   var inputs = cmd.split(' ');
@@ -19,8 +21,19 @@ function parseCmd(cmd) {
 function echo(inputs) {
   parseResponse(inputs.slice(1).join(' '));
 }
+function cat(inputs){
+  readFile(inputs[1],function(contents){
+    parseResponse(contents);
+    if(inputs.length > 2) cat(inputs.slice(1));
+  });
+}
+//  fs.readFile('./' + inputs[1],function(err,contents){
+//    if(err) throw err;
+//    parseResponse(contents);
+//    if(inputs.length > 2) cat(inputs.slice(1));
+//  });
 
-function ls() {
+function ls(inputs) {
   fs.readdir('.', function(err, files){
     if(err) throw err;
     var result = [];
@@ -30,6 +43,38 @@ function ls() {
     parseResponse(result.join(" "));
   });
 }
+function head(inputs){
+  readFile(inputs[1],function(contents){
+    var lines = new String(contents).split("\n");
+    parseResponse(lines.slice(0,5).join("\n"));
+  });
+}
+function tail(inputs){
+  readFile(inputs[1],function(contents){
+    var result = new String(contents).split("\n").filter(function(line, index, array){
+      return index > array.length - 6;
+    }).join("\n");
+    parseResponse(result);
+  });
+}
+function sort(inputs){
+  readFile(inputs[1],function(contents){
+    var result = new String(contents).split("\n").sort(function(a, b){
+      if (a.toLowerCase() > b.toLowerCase()) return 1;
+      if (a.toLowerCase() < b.toLowerCase()) return -1;
+      if (a.toLowerCase() === b.toLowerCase()) return 0;
+    }).join("\n");
+    parseResponse(result);
+  });
+}
+
+function readFile(input, f){
+  fs.readFile('./' + input, function(err,contents){
+    if (err) throw err;
+    f(contents);
+  });
+}
+
 function parseResponse(response){
   process.stdout.write(response + "\nprompt >");
 }
