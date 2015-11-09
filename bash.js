1b62b101235f5213d6f4f66475079cc42432fdaf
Nick <npalenchar@gmail.com>
2015-11-09 16:13:20 -0500

    implimented ls/refactored funcs with helper func 'parseResponse'

--- bash.js
+++ bash.js
@@ -6,8 +6,6 @@ process.stdout.write('prompt > ');
 // the stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
   var cmd = data.toString().trim(); // remove the newline
-
-  process.stdout.write(commands.parseCmd(cmd));
-
-  process.stdout.write('\nprompt > ');
+  commands.parseCmd(cmd)
+//  process.stdout.write(commands.parseCmd(cmd));
 });
--- commands.js
+++ commands.js
@@ -1,11 +1,27 @@
- var commands = {
-  pwd: function() { return __dirname; },
-  date: function() { return Date(); },
-  default: function(cmd) { return 'You typed: ' + cmd; }
-}
+var fs = require('fs');
 
+var commands = {
+  pwd: function() { parseResponse(__dirname); },
+  date: function() { parseResponse(Date()); },
+  ls: ls,
+  default: function(cmd) { parseResponse('You typed: ' + cmd); }
+}
 function parseCmd(cmd) {
+  
   return (commands[cmd] || commands['default'])(cmd);
+}
+function ls() {
+  fs.readdir('.', function(err, files){
+    if(err) throw err;
+    var result = [];
+    files.forEach(function(file){
+      result.push(file.toString());
+    });
+    parseResponse(result.join(" "));
+  });
+}
+function parseResponse(response){
+  process.stdout.write(response + "\nprompt >");
 }
 
 module.exports.parseCmd = parseCmd;
